f6410e9b2f0d59ad9571c9781bedb013bb8606ef
Vinicius Gabriel <vinihbieel49@gmail.com>
2023-04-02 13:34:46 -0300

    added Login screen

--- src/Navigator.js
+++ src/Navigator.js
@@ -2,10 +2,19 @@ import React from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer,  } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AddPhoto from './screens/AddPhoto';
 import Profile from './screens/Profile';
+import { createSwitchNavigator } from '@react-navigation/compat';
+import Login from './screens/Login';
+const loginOrProfileRouter = createSwitchNavigator({
+     Perfil: Profile,
+     Auth: Login
+ }, {
+   initialRouteName: 'Profile'
+ })
+ 
 
 export default function MenuNavigator() {
      const Tab = createBottomTabNavigator();
@@ -26,7 +35,7 @@ export default function MenuNavigator() {
                                    <Icon name='camera' size={30} color={color} />
                               ),
                          }} />
-                    <Tab.Screen name="Profile" component={Profile}
+                    <Tab.Screen name="Profile" component={loginOrProfileRouter}
                          options={{
                               tabBarShowLabel: false,
                               tabBarIcon: ({ color }) => (
