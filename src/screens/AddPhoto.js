13adcc037c11c987612bc8d09ee8d9c9d3d88927
Vinicius Gabriel <vinihbieel49@gmail.com>
2023-04-03 19:01:58 -0300

    adding validators

--- src/screens/AddPhoto.js
+++ src/screens/AddPhoto.js
@@ -1,168 +1,3 @@
-/* import { FontAwesome } from '@expo/vector-icons';
-import * as ImagePicker from 'expo-image-picker';
-import React, { useState } from 'react';
-import { connect } from 'react-redux'
-import { addPost } from '../store/actions/posts'
-
-import {
-  Alert,
-  Dimensions,
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-
-const AddPhoto = () => {
-  const [image, setImage] = useState(null);
-  const [comment, setComment] = useState('');
- 
-  const pickImage = async (camera = false) => {
-    const config = {
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    };
- 
-    try {
-      let result = camera
-        ? await ImagePicker.launchCameraAsync(config)
-        : await ImagePicker.launchImageLibraryAsync(config);
- 
-      if (!result.canceled) {
-        setImage({ uri: result.uri });
-      }
-    } catch (E) {
-      Alert.alert('Erro ao tentar carregar imagem.');
-    }
-  };
- 
-  const save = async (props) => {
-    Alert.alert('Imagem adicionada!', comment);
-    addPost({
-      id: Math.random(),
-      nickname: props.name,
-      email: props.email,
-      image: image,
-      comments: [{
-        nickname: props.name,
-        comment: ` ${comment}`
-    }]
-    })
-  };
-
-
- 
-  return (
-    <ScrollView>
-      <View style={styles.container}>
-        <Text style={styles.title}>Compartilhe uma imagem</Text>
-        <View style={styles.imageContainer}>
-          <Image source={image} style={styles.image} />
-        </View>
-        <TextInput
-          placeholder='Algum comentário para a foto?'
-          style={styles.input}
-          value={comment}
-          onChangeText={setComment}
-        />
- 
-        <View style={styles.optionsContainer}>
-          <View style={styles.buttomContainer}>
-            <TouchableOpacity onPress={() => pickImage()} style={styles.option}>
-              <FontAwesome name='photo' size={30} color='#000' />
-              <Text>Galeria</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => pickImage(true)} style={styles.option}>
-              <FontAwesome name='camera' size={30} color='#000' />
-              <Text>Câmera</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={save} style={styles.option}>
-              <FontAwesome name='save' size={30} color='#000' />
-              <Text>Salvar</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    </ScrollView>
-  );
-};
- 
- 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    marginTop: 20,
-  },
-  title: {
-    fontSize: 20,
-    marginTop: Platform.OS === 'ios' ? 30 : 10,
-    fontWeight: 'bold',
-  },
-  imageContainer: {
-    width: '90%',
-    height: Dimensions.get('window').width / 2,
-    backgroundColor: '#EEE',
-    marginTop: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  image: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').width / 2,
-    resizeMode: 'center',
-  },
-  optionsContainer: {
-    flexDirection: 'column',
-    alignContent: 'center',
-    width: '90%',
-    marginTop: 30,
-    padding: 5,
-  },
-  buttomContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: 'auto',
-    padding: 10,
-  },
-  option: {
-    alignItems: 'center',
-  },
-  buttomText: {
-    fontSize: 20,
-    color: '#FFF',
-  },
-  input: {
-    marginTop: 20,
-    width: '90%',
-  },
-});
-
-const mapStateToProps = ({ user }) => {
-  return {
-      email: user.email, 
-      name: user.name
-  }
-}
-
-
-const mapDispatchToProps = dispatch => {
-  return {
-      onAddPost: post => dispatch(addPost(post))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(AddPhoto)
-
-
- */
-
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addPost } from '../store/actions/posts'
@@ -180,6 +15,7 @@ import {
 } from 'react-native'
 import * as ImagePicker from 'expo-image-picker';
 import { Permissions } from 'expo';
+const noUser = 'Você precisa estar logado para adicionar imagens';
 class AddPhoto extends Component {
   state = {
     image: null,
@@ -187,6 +23,10 @@ class AddPhoto extends Component {
   }
 
   pickImage = async (camera = false) => {
+    if (!this.props.name) {
+      Alert.alert('Falha!', noUser);
+      return;
+    };
     const config = {
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
@@ -215,6 +55,11 @@ class AddPhoto extends Component {
 
 
   save = async () => {
+    if (!this.props.name) {
+      Alert.alert('Falha!', noUser);
+      return;
+    };
+    
     this.props.onAddPost({
       id: Math.random(),
       nickname: this.props.name,
