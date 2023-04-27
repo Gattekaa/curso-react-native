911469648309953db88aaeb479b77ff6f2de7e4a
Vinicius Gabriel <vinihbieel49@gmail.com>
2023-04-27 17:08:01 -0300

    buttons

--- App.js
+++ App.js
@@ -1,50 +1,65 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'; 
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+class App extends Component{
+  constructor(props){
+    super(props);
     this.state = {
-      nome: ''
-    }
-    this.pegaNome = this.pegaNome.bind(this)
+      nome: '',
+      input: ''
+    };
+
+    this.entrar = this.entrar.bind(this);
   }
 
-  pegaNome(texto) {
-    if(texto.length > 0){
-      this.setState({nome: 'Bem vindo: ' + texto})
-    } else {
-      this.state({nome: ''})
+  entrar(){
+    if(this.state.input === ''){
+      alert('Digite seu nome!');
+      return;
     }
+    
+    this.setState({nome: 'Bem vindo: ' + this.state.input});
   }
-  render() {
-    return (
-      <View style={styles.area} >
 
-        <TextInput onChangeText={this.pegaNome} style={styles.input} placeholder='Digite seu nome!' underlineColorAndroid="transparent" />
+
+  render(){
+    return(
+      <View style={styles.container}>
+
+        <TextInput
+        style={styles.input}
+        placeholder="Digite seu nome?"
+        underlineColorAndroid="transparent"
+        onChangeText={ (texto) => this.setState({input: texto}) }
+        />
+
+        <Button title="Entrar" onPress={this.entrar} />
 
         <Text style={styles.texto}>{this.state.nome}</Text>
+
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
+  container:{
+    flex:1,
   },
-  input: {
+  input:{
     height: 45,
     borderWidth: 1,
     borderColor: '#222',
     margin: 10,
     fontSize: 20,
-    padding: 10
+    padding: 10,
   },
-  texto: {
+  texto:{
     textAlign: 'center',
     fontSize: 25
   }
 });
+
+
 
 export default App;
